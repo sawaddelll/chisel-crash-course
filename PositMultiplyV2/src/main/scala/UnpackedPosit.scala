a36a18990b8028ee22e1461687b614cc5ef6c451
Sam Waddell <38145503+sawaddelll@users.noreply.github.com>
2019-07-18 14:19:50 -0700

    Add files via upload

---
+++ PositMultiplyV2/src/main/scala/UnpackedPosit.scala
@@ -0,0 +1,29 @@
+// See README.md for license details.
+package PositDef
+
+import chisel3._
+//import chisel3.util._
+import chisel3.core.Bundle
+import PositDef._
+
+class UnpackedPosit (width: Int, es : Int) extends Bundle {
+  val isZero = Bool()
+  val isInf  = Bool()
+  val sign   = Bool()
+  val exponent = UInt(PositDef.getUnsignedExponentBits(width, es).W) //.5.W/
+  val fraction = UInt(PositDef.getFractionBits(width, es).W) //4.W
+
+  override def cloneType =
+    new UnpackedPosit(width, es).asInstanceOf[this.type]
+
+}
+
+object UnpackedPositFunctions {
+  def signedRegime(unpackedPosit: UnpackedPosit, width: Int, es: Int): SInt = {
+    val BITS: Int = PositDef.getUnsignedRegimeBits(width)
+    val answer = SInt(PositDef.getSignedRegimeBits(width).W)
+    answer := (unpackedPosit.exponent(PositDef.getUnsignedExponentBits(width, es) - 1, es) -
+      PositDef.getMaxSignedRegime(width)(BITS.W)).asSInt()
+    return answer
+  }
+}
