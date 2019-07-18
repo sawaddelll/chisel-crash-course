b5a09cd14abedf4c48dd8b7dea24197d55b15b48
Sam Waddell <38145503+sawaddelll@users.noreply.github.com>
2019-07-18 14:54:38 -0700

    Update Test.scala
    Changed test to add encoding/decoding

--- PositMultiplyV2/src/test/scala/Test.scala
+++ PositMultiplyV2/src/test/scala/Test.scala
@@ -7,36 +7,61 @@ import example._
 
 class DataGen(width: Int = 8,  es: Int = 1) extends Module {
   val io = IO(new Bundle {
-    val a = Output(new UnpackedPosit(width = 8, es = 1)).asOutput
-    val b = Output(new UnpackedPosit(width = 8, es = 1)).asOutput
+    val a = Output(new UnpackedPosit(width = width, es = es))
+    val b = Output(new UnpackedPosit(width = width, es = es))
   })
+
+
+  /*TESTING 5 * 8 = 40
+  01100010 * 01101000 = 01110101
+
+   */
+  val aPacked = Wire(new PackedPosit(width = width, es = es))
+  val aBits = Bits("b01100010")
+  //aBits := 01100010.Bits
+  aPacked.bits := aBits
+
+  val bPacked = Wire(new PackedPosit(width = width, es = es))
+  val bBits = Bits("b01101000")
+  //aBits := 01100010.Bits
+  bPacked.bits := bBits
   
+  val decoding = new PositDecode(width = width, es = es)
+  decoding.io.in := aPacked
+  io.a := decoding.io.out
 
-  io.a.isZero  := 0.U
-  io.a.isInf   := 0.U
-  io.a.sign    := 1.U
-
-  io.b.sign  := 1.U
-  io.b.isInf   := 0.U
-  io.b.isZero  := 0.U
-
-  io.a.exponent := 5.U(5.W)
-  io.a.fraction := 0.U(4.W)
-
-  io.b.exponent := 10.U(5.W)
-  io.b.fraction := 5.U(4.W)
+  val decodingB = new PositDecode(width = width, es = es)
+  decoding.io.in := bPacked
+  io.b := decoding.io.out
 }
 
 class Test extends Module {
   val io = IO(new Bundle {})
-  val gen = Module(new DataGen(width = 8, es = 1))
-  val multiply = Module(new PositMultiply(width = 8,es = 1, trailing_bits = 2))
+  val width: Int = 8
+  val es: Int = 1
+  val gen = Module(new DataGen(width, es))
+  val multiply = Module(new PositMultiply(width, es, trailing_bits = 2))
   multiply.io.a <> gen.io.a
   multiply.io.b <> gen.io.b
   
-  val trailingBits = Wire(UInt(2.W))
+  val encodingInputA = new PositEncode(width = width, es)
+  encodingInputA.io.in := gen.io.a
+  val aValue = Wire(new PackedPosit(width = width, es = es))
+  aValue <> encodingInputA.io.out
+
+  val encodingInputB = new PositEncode(width = width, es)
+  encodingInputB.io.in := gen.io.b
+  val bValue = Wire(new PackedPosit(width = width, es = es))
+  bValue <> encodingInputB.io.out
+
+  val encodingOutput = new PositEncode(width = width, es)
+  encodingOutput.io.in := multiply.io.out
+  val outputValue = Wire(new PackedPosit(width = width, es = es))
+  outputValue <> encodingOutput.io.out
+
+  val trailingBits = Wire(UInt(8.W))
   val stickyBit = Wire(UInt(1.W))
-  
+
   trailingBits := multiply.io.trailingBits
   stickyBit := multiply.io.stickyBit
 
@@ -44,12 +69,12 @@ class Test extends Module {
 
   output <> multiply.io.out
 
-//  while(true) {
+  while(true) {
     printf("a.exponent is %b \n",gen.io.a.exponent)
     printf("a.fraction is %b \n", gen.io.a.fraction)
     printf("b.exponent is %b \n", gen.io.b.exponent)
     printf("b.fraction is %b \n", gen.io.b.fraction)
-  
+
     printf("out.sign is %b \n", multiply.io.out.sign)
     printf("out.isZero is %b \n", multiply.io.out.isZero)
     printf("out.isInf is %b \n", multiply.io.out.isInf)
@@ -59,11 +84,12 @@ class Test extends Module {
 
     printf("out.exponent is %b \n", multiply.io.out.exponent)
     printf("out.fraction is %b \n", multiply.io.out.fraction)
+    
+    printf("a is %b", aValue.bits)
+    printf("b is %b", bValue.bits)
+    printf("out is %b", outputValue.bits)
 
- // }
+  }
 
 }
 
-object Elaborate extends App {
-  chisel3.Driver.execute(args, () => new Test)
-}
