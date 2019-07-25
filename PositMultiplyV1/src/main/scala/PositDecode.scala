383ecdce9b292fe4e16b59da5fee432386d83366
Sam Waddell <38145503+sawaddelll@users.noreply.github.com>
2019-07-25 16:31:42 -0700

    Update PositDecode.scala

--- PositMultiplyV1/src/main/scala/PositDecode.scala
+++ PositMultiplyV1/src/main/scala/PositDecode.scala
@@ -84,10 +84,13 @@ class PositDecode(width: Int = 8, es: Int = 1) extends Module{
   } .otherwise {
     when ((regimePosOrZero === 1.U).asBool()) {
       unsignedRegime := cl0extended
+      printf("unsignedRegime is %b (statement by unReg assignment)", unsignedRegime)
     } .otherwise {
       //is ~ actually wrong, or just intelliJ?????????????????????????
       unsignedRegime := LOCAL_MAX_SIGNED_REGIME.U/*(LOCAL_UNSIGNED_REGIME_BITS.W)*/ + ~cl0/*extended*/
+      printf("unsignedRegime is %b (statement by unReg assignment)", unsignedRegime)
     }
+    
   }
 
   // Can we just do this?
@@ -133,6 +136,7 @@ class PositDecode(width: Int = 8, es: Int = 1) extends Module{
 
     io.out.fraction := esAndFractionBits(LOCAL_MAX_REGIME_FIELD_SIZE-3-es, 0)
     io.out.exponent := Cat(unsignedRegime, esBits)
+    printf("unsignedRegime is %b (statement by exp assignment)", unsignedRegime)
 
   } else {
     // There is no ES to extract
